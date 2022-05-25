bd1d339a410d40c6f38f97fa520f0de16c1c46fa
Harrison <harrison.bradley.turpin@gmail.com>
2022-05-25 13:55:54 +0100

    RCON Kick/Ban Buttons
    Not functional yet, just setting up

--- public/js/app/views/servers/players.js
+++ public/js/app/views/servers/players.js
@@ -5,6 +5,24 @@ var tpl = require('tpl/servers/players.html')
 
 module.exports = Marionette.LayoutView.extend({
   template: _.template(tpl),
+
+  events: {
+    'click #kick': 'kick',
+    'click #ban': 'ban'
+  },
+
+
+  // Need to make this a two parter list item thing so the buttons can refer to the player they are nested in.
+  // 
+  kick: function (event) {
+    var self = this
+  },
+
+
+
+
+
+
   templateHelpers: {
     players: function () {
       return _.sortBy(this.state.players, function (player) {
