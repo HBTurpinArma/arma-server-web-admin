98d21cda757903fdfb5b8a5e8be490785446744a
Harrison <harrison.bradley.turpin@gmail.com>
2024-04-09 17:08:42 +0100

    Server Game Ordering

--- public/js/app/collections/servers.js
+++ public/js/app/collections/servers.js
@@ -4,7 +4,7 @@ var Server = require('app/models/server')
 
 module.exports = Backbone.Collection.extend({
   comparator: function (a, b) {
-    return a.get('title').toLowerCase().localeCompare(b.get('title').toLowerCase())
+    return (a.get('game_selected')+a.get('title')).toLowerCase().localeCompare((b.get('game_selected')+b.get('title')).toLowerCase())
   },
   model: Server,
   url: '/api/servers/'
